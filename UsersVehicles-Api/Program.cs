a73bb3388d77ef41032bfe2da8150dbe0e1a4c35
LázaroK <lazarolk23@hotmail.com>
2025-10-22 14:16:28 -0300

    feat: implementa endpoint para login simples com credencias hardcoded (só para teste)

--- UsersVehicles-Api/Program.cs
+++ UsersVehicles-Api/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.AspNetCore.Http;
+using UsersVehicles_Api.Dominio.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDto) =>
+{
+    if (loginDto.Email == "superadmin@gmail.com" && loginDto.Senha == "1234567")
+    {
+        return Results.Ok("Login realizado com sucesso!");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
 
 app.Run();
