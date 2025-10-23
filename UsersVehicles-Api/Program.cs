e89057b7488627fd4d497a9c9c1fa9b591bacb80
LázaroK <lazarolk23@hotmail.com>
2025-10-23 03:34:08 -0300

    feat: Implementa uso do swagger para documentação, adiciona view Home e cria rota Post para Veiculo com injeção do servico e uso de DTO

--- UsersVehicles-Api/Program.cs
+++ UsersVehicles-Api/Program.cs
@@ -2,26 +2,37 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UsersVehicles_Api.Dominio.DTOs;
+using UsersVehicles_Api.Dominio.Entidades;
 using UsersVehicles_Api.Dominio.Interfaces;
+using UsersVehicles_Api.Dominio.ModelViews;
 using UsersVehicles_Api.Dominio.Servicos;
 using UsersVehicles_Api.Infraestrutura.Db;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 //P injeção de dependência
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DbVehiclesContext>(options =>
 {
     options.UseNpgsql(builder.Configuration
     .GetConnectionString("PostgreSQLConnection")); 
 });
-
 var app = builder.Build();
+#endregion
 
-app.MapGet("/", () => "Hello World!");
+#region Home
+app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-app.MapPost("/login", ([FromBody]LoginDTO loginDto, IAdministradorServico administradorServico) =>
+#region Administradores
+app.MapPost("/login", ([FromBody] LoginDTO loginDto, IAdministradorServico administradorServico) =>
 {
     if (administradorServico.Login(loginDto) != null)
     {
@@ -32,5 +43,26 @@ app.MapPost("/login", ([FromBody]LoginDTO loginDto, IAdministradorServico admini
         return Results.Unauthorized();
     }
 });
+#endregion
+
+#region Veiculos
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDto, IVeiculoServico veiculoServico) =>
+{
+    var veiculo = new Veiculo
+    {
+        Nome = veiculoDto.Nome,
+        Marca = veiculoDto.Marca,
+        Ano = veiculoDto.Ano
+    };
+    veiculoServico.Incluir(veiculo);
+
+    return Results.Created($"/veiculos/{veiculo.Id}", veiculo);  
+});
+#endregion
+
+#region App
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
+#endregion
